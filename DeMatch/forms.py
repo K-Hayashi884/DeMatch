173ff8eb6d6c28749af959373a6954c4c43eb8cf
shunki <bonoshunki@gmail.com>
2020-12-09 21:15:17 +0900

    groupcreateのviewsを作成

--- DeMatch/forms.py
+++ DeMatch/forms.py
@@ -19,12 +19,12 @@ class CreateGroupForm(ModelForm):
     if user.friends():  
       self.fields['inviting'].queryset = User.objects.get(user=user).friends
 
-  def create(self):
-    name = self.cleaned_data['name']
-    image = self.cleaned_data['image']
-    hobby = self.cleaned_data['hobby']
-    subject = self.cleaned_data['subject']
-    introduction = self.cleaned_data['introduction']
-    inviting = self.cleaned_data['inviting']
-    group = Group(name=name, image=image, hobby=hobby, subject=subject, introduction=introduction, inviting=inviting)
-    group.save()
+  # def create(self):
+  #   name = self.cleaned_data['name']
+  #   image = self.cleaned_data['image']
+  #   hobby = self.cleaned_data['hobby']
+  #   subject = self.cleaned_data['subject']
+  #   introduction = self.cleaned_data['introduction']
+  #   inviting = self.cleaned_data['inviting']
+  #   group = Group(name=name, image=image, hobby=hobby, subject=subject, introduction=introduction, inviting=inviting)
+  #   group.save()
--- DeMatch/views.py
+++ DeMatch/views.py
@@ -2,14 +2,21 @@ from django.shortcuts import render
 from .models import User, Hobby, Subject, UserFriendRelation, UserImg, Group
 from .forms import CreateGroupForm
 from django.views import generic
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 #groupの作成
 class GroupCreateView(LoginRequiredMixin, generic.CreateView):
-  model = Group
-  template_name = 'create_group.html'
-  from_class = CreateGroupForm
-  success_url = reverse_lazy()
+    model = Group
+    template_name = 'create_group.html'
+    from_class = CreateGroupForm
+    success_url = reverse_lazy()
+
+    def form_valid(self, form):
+      diary = form.save()
+      return super().form_valid(form)
+    def form_invalid(self, form):
+        messages.error(self.request, "Groupの作成に失敗しました。")
 
   
 def home(request):
