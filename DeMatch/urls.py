1ea60b2142139bcc40c4162be51bd243719e2ece
shunki <bonoshunki@gmail.com>
2020-12-11 01:04:58 +0900

    GroupDetailViewの作成

--- DeMatch/urls.py
+++ DeMatch/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "DeMatch"
 urlpatterns = [
     path("", views.home, name="home"),    
-    path('create_group', views.CreateGroupView, name='create_group')
+    path("create_group/", views.GroupCreateView.as_view(), name="create_group"),
+    path("group_detail/<int:pk>/", views.GroupDetailView, name="group_detail"),
 ]
--- DeMatch/views.py
+++ DeMatch/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import User, Hobby, Subject, UserFriendRelation, UserImg, Group
 from .forms import CreateGroupForm
+from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -10,13 +11,22 @@ class GroupCreateView(LoginRequiredMixin, generic.CreateView):
     model = Group
     template_name = 'create_group.html'
     from_class = CreateGroupForm
-    success_url = reverse_lazy()
+    success_url = reverse_lazy('group_detail')
 
     def form_valid(self, form):
       diary = form.save()
       return super().form_valid(form)
     def form_invalid(self, form):
         messages.error(self.request, "Groupの作成に失敗しました。")
+
+#groupの詳細
+#id一致で取得。id情報はurlに組み込む
+def GroupDetailView(request, pk):
+    group = Group.objects.get(id=pk)
+    params = {
+        'group':group,
+    }
+    return render(request, "DeMatch/group_detail.html", params)
 
   
 def home(request):
