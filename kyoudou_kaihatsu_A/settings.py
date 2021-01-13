677e60ca179960726dc930bac512ee1015f58776
shunki <bonoshunki@gmail.com>
2021-01-13 21:18:41 +0900

    a

--- kyoudou_kaihatsu_A/settings.py
+++ kyoudou_kaihatsu_A/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -188,7 +188,7 @@ try:
 except ImportError:
     pass
 
-if not DEBUG:
-    import django_heroku
-    django_heroku.settings(locals())
+
+import django_heroku
+django_heroku.settings(locals())
 
